5c0ef00c510182b977f74afd1e262ec9adddceeb
CJ-programming <86181787+CJ-programming@users.noreply.github.com>
2023-11-06 14:41:41 +1100

    Update crypto_utils.py

--- src/miner_client/crypto_utils.py
+++ src/miner_client/crypto_utils.py
@@ -1,6 +1,60 @@
+from Crypto.Cipher.AES import new, MODE_CBC, block_size
+from Crypto.Util.Padding import pad
+
 from ecdsa import SECP256k1
+from ecdsa import SigningKey
 from ecdsa import VerifyingKey
 from ecdsa.util import number_to_string
+
+from hashlib import sha256
+
+from os.path import exists
+from os.path import getsize
+
+def create_private_key(key):
+    if exists('private_key.bin') and getsize('private_key.bin') > 0:
+        return
+
+    with open('private_key.bin', 'wb') as f:
+        private_key = SigningKey.generate(SECP256k1).to_string()
+
+        cipher = new(key, MODE_CBC)
+
+        ciphered_data = cipher.encrypt(pad(private_key, block_size))
+
+        f.write(cipher.iv)
+        f.write(ciphered_data)
+
+def double_sha256(data):
+    double_sha256_data = sha256(sha256(data).digest()).digest()
+    return double_sha256_data
+
+def calculate_merkle_root(txs):
+    if len(txs) == 0:
+        return b'\x00'*32
+
+    if len(txs) == 1:
+        return double_sha256(txs[0])
+
+    new_tx_list = []
+
+    for i in range(0, len(txs), 2):
+        tx_hash1 = double_sha256(txs[i])
+
+        if i + 1 == len(txs):
+            tx_hash2 = tx_hash1
+        else:
+            tx_hash2 = double_sha256(txs[i+1])
+
+        new_tx_list.append(tx_hash1 + tx_hash2)
+
+    return calculate_merkle_root(new_tx_list)
+
+def nbits_to_target(nbits):
+    exponent = nbits >> 24
+    mantissa = nbits & 0xffffff
+    target = mantissa * 256 ** (exponent-3)
+    return target
 
 def compress_verifying_key(verifying_key : VerifyingKey) -> bytes:
     x = verifying_key.pubkey.point.x()
@@ -12,19 +66,17 @@ def compress_verifying_key(verifying_key : VerifyingKey) -> bytes:
 def generate_outputs(own_address, inputs, amount, address, fee):    
     """
     input format:
-    [{txid, output_index, value, address}]
+    [{txid, output_index, amount, address}]
     """
 
-    utxos_balance = sum(i['value'] for i in inputs)
- 
-    if utxos_balance >= amount:
-        outputs = []
+    outputs = []
 
-        total_amount = amount - fee
+    utxos_balance = sum(i['amount'] for i in inputs)
 
-        change = utxos_balance - total_amount
+    if utxos_balance >= (amount + fee):
+        change = (utxos_balance - amount) - fee
 
-        outputs.append({'value' : total_amount, 'address' : address})
-        outputs.append({'value' : change, 'address' : own_address})
+        outputs.append({'amount' : amount, 'address' : address})
+        outputs.append({'amount' : change, 'address' : own_address})
     
     return outputs
